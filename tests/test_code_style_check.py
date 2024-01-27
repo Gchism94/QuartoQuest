90b4eba2bbdc7ba53283b39f16b3df64c032f5ed
Greg T. Chism <gchism@email.arizona.edu>
2024-01-27 13:21:53 -0700

    Create test_code_style_check.py

---
+++ tests/test_code_style_check.py
@@ -0,0 +1,12 @@
+import unittest
+from code_style_check import check_code_style
+
+class TestCodeStyleCheck(unittest.TestCase):
+    def test_linting(self):
+        code_blocks = ["import math\nx=2\nprint(x)"]  # Incorrect style
+        results = check_code_style(code_blocks)
+        self.assertNotEqual(results[0], "")  # Expecting non-empty result due to style issues
+
+# Run the tests
+if __name__ == '__main__':
+    unittest.main()
