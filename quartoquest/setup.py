e6067118eb54e53fb71f2c608e6a30319f13309d
Greg T. Chism <gchism@email.arizona.edu>
2024-01-27 14:27:02 -0700

    Update setup.py

--- quartoquest/setup.py
+++ quartoquest/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='QuartoQuest',
+    name='quartoquest',
     version='0.1.0',  # Replace with your version
     packages=find_packages(),
     install_requires=[
