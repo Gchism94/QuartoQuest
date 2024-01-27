0eb0e5ceb4b89b8205101c83bd31f10f9d349fd0
Greg T. Chism <gchism@email.arizona.edu>
2024-01-27 13:58:32 -0700

    Create setup.py

---
+++ quartoquest/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QuartoQuest',
+    version='0.1.0',  # Replace with your version
+    packages=find_packages(),
+    install_requires=[
+        # List dependencies here
+    ],
+)
