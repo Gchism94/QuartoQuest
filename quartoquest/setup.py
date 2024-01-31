03c6e8e57afb0fece6dca32de2e148d290385b79
Greg T. Chism <gchism@email.arizona.edu>
2024-01-31 02:02:07 +0000

    Fix setup.py

--- quartoquest/setup.py
+++ quartoquest/setup.py
@@ -10,7 +10,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'quartoquest-autograder = quartoquest.autograder:main',
+            'autograder = quartoquest.autograder:main',
         ],
     },
 )
