6faa8541afbd81dd5b3c651ea682247e74bafeb4
Gchism94 <gchism@arizona.edu>
2024-03-01 16:01:08 -0700

    Same bug as before, fixing absolute paths in autograder test

--- tests/test_autograder.py
+++ tests/test_autograder.py
@@ -5,16 +5,16 @@ import os
 
 class TestAutograder(unittest.TestCase):
 
-    @patch('autograder.os.getenv', return_value='.')
-    @patch('autograder.glob.glob', return_value=['/path/to/notebook.ipynb'])
-    @patch('autograder.check_directory_structure', return_value={'missing_directories': [], 'unexpected_files': []})
-    @patch('autograder.parse_ipynb', return_value={'code_cells': ['print("Hello, World!")'], 'markdown_cells': ['# Title']})
-    @patch('autograder.assess_code_quality', return_value={'Code Block 1': {'Complexity': 'Low', 'Structure': 'Good'}})
-    @patch('autograder.check_code_style', return_value=['No issues found.'])
-    @patch('autograder.analyze_commit_messages', return_value={'total_commits': 1, 'short_message_issues': 0, 'non_informative_issues': 0, 'non_conforming_messages': 1})
-    @patch('autograder.check_security_vulnerabilities', return_value='No security vulnerabilities found.')
-    @patch('autograder.check_dependencies', return_value='All dependencies are secure.')
-    @patch('autograder.generate_html_with_css')
+    @patch('jupyterquest.autograder.os.getenv', return_value='.')
+    @patch('jupyterquest.autograder.glob.glob', return_value=['/path/to/notebook.ipynb'])
+    @patch('jupyterquest.autograder.check_directory_structure', return_value={'missing_directories': [], 'unexpected_files': []})
+    @patch('jupyterquest.autograder.parse_ipynb', return_value={'code_cells': ['print("Hello, World!")'], 'markdown_cells': ['# Title']})
+    @patch('jupyterquest.autograder.assess_code_quality', return_value={'Code Block 1': {'Complexity': 'Low', 'Structure': 'Good'}})
+    @patch('jupyterquest.autograder.check_code_style', return_value=['No issues found.'])
+    @patch('jupyterquest.autograder.analyze_commit_messages', return_value={'total_commits': 1, 'short_message_issues': 0, 'non_informative_issues': 0, 'non_conforming_messages': 1})
+    @patch('jupyterquest.autograder.check_security_vulnerabilities', return_value='No security vulnerabilities found.')
+    @patch('jupyterquest.autograder.check_dependencies', return_value='All dependencies are secure.')
+    @patch('jupyterquest.autograder.generate_html_with_css')
     @patch('builtins.open', new_callable=unittest.mock.mock_open)  # Mock the built-in open function
     def test_main_integration(self, mock_open, mock_generate_html, mock_check_deps, mock_run_security, mock_analyze_commits, mock_check_style, mock_assess_quality, mock_parse, mock_check_structure, mock_glob, mock_getenv):
         # Setup mocks
